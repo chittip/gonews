7de204cd493f90e1166cfe72556f71aaaacc1ce6
chittip <chittip@gmail.com>
2017-07-10 15:44:54 +0700

    init

---
+++ pkg/app/admin.go
@@ -0,0 +1,20 @@
+package app
+
+import "net/http"
+import "github.com/chittip/gonews/pkg/view"
+
+func adminLogin(w http.ResponseWriter, r *http.Request) {
+	view.AdminLogin(w, nil)
+}
+
+func adminList(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func adminEdit(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func adminCreate(w http.ResponseWriter, r *http.Request) {
+
+}
